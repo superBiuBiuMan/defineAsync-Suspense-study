bd2b1d325030e9588c9ffc973dce0c71a55df38b
qiuye <1464815392@qq.com>
2023-10-18 19:42:42 +0800

    feat:defineAsyncComponent示例

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<style scoped>
+
+</style>
+<template>
+  <button @click="handleHome">切换到首页</button>
+  <button @click="handleCenter">切换到个人中心页面</button>
+  <AsyncHome v-if="isShowHome"/>
+  <AsyncCenter v-if="!isShowHome"/>
+</template>
+<script setup lang="ts">
+import {defineAsyncComponent,ref} from "vue";
+import Loading from "./components/Loading/index.vue"
+
+const AsyncHome = defineAsyncComponent({
+  loader: () => import("./components/Home/index.vue"),
+  loadingComponent:Loading,
+  delay:200,
+})
+
+const AsyncCenter = defineAsyncComponent({
+  loader: async () => {
+    await new Promise(resolve => {
+      setTimeout(() => {resolve()},2000);
+      //加载5秒后返回
+    })
+    return import("./components/Center/index.vue")
+  },
+  loadingComponent:Loading,
+  delay:200,
+})
+
+const isShowHome = ref<boolean>(true);
+
+const handleHome = () => {
+  isShowHome.value = true;
+}
+
+const handleCenter = () => {
+  isShowHome.value = false;
+}
+</script>
---
+++ src/components/Loading/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    加载中
+  </div>
+</template>
+
+<style scoped>
+
+</style>
